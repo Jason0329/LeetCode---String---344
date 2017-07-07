74e93ee45b9c4e325d7169510ffce924fc9af9ce
啟昇 李 <a3261478@hotmail.com>
2017-07-07 21:05:21 +0800

    Add project files.

---
+++ LeetCode - String - 344/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode___String___344
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution ss = new Solution();
+            Console.Write(ss.ReverseString("Hello World!"));
+        }
+    }
+
+    public class Solution
+    {
+        public string ReverseString(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = s.Length; i >= 1; i--)
+            {
+                sb.Append(s[i-1]);
+            }
+            return sb.ToString();
+
+        }
+    }
+}
